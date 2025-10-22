4d6895e9dc8301fd417edec3e0172ad5ff7d05c9
Valeria Makarevich <124881534+ValeriaMakarevich@users.noreply.github.com>
2025-10-22 15:01:57 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 
 import { Route, Routes } from 'react-router'
 import './App.css'
-import { PagesResipe } from './Pages/PagesResipe/PagesResipe'
-import { MainPage } from './Pages/MainPage/MainPage';
+import { PagesResipe } from './pages/PagesResipe/PagesResipe'
+import { MainPage } from './pages/mainPage/MainPage';
 
 
 
